c88481c3c0fa711958290f5617b21b9b25edce8c
Tyler Fuqua <tyler@agilityda.com>
2017-12-25 18:29:51 -0700

    Hands On Project 2

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // Hands On Project 1
 
-var amazingCars = ["BMW", "Lexus","Ford", "Mercedes" ]
+/*var amazingCars = ["BMW", "Lexus","Ford", "Mercedes" ]
     amazingCars.push("Honda") 
-        console.log(amazingCars);
+        console.log(amazingCars);*/
     
 
+// Hands On Project 2
+
+var awesomeBirds = ["Bluebird", "Roadrunner", "Dedert Dove", "Pigeon"]
+    awesomeBirds.pop([3]);
+    awesomeBirds.sort();
+    awesomeBirds.length;
+
+  
